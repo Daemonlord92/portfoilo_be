39db884b5047f49727ec8657bf531231914672c1
Matthew Jeshua Martin <zhintateo19924559@gmail.com>
2020-08-10 21:59:52 -0500

    Created Seed Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Project.create(title: 'Developer Portfoilo', proj_img: 'https://reactstrap.github.io/assets/logo.png', tools: 'React, Rails, Devise, JWT Token, Redux')
+puts 'MJM: seeds.rb: Created 1 Project Seed Data'
