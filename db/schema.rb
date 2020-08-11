218794cfe0a0d32eea64dfdca88dabb0791e790c
Matthew Jeshua Martin <zhintateo19924559@gmail.com>
2020-08-11 00:27:46 -0500

    setup seed data for blog

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_11_013937) do
+ActiveRecord::Schema.define(version: 2020_08_11_051207) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "post"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
